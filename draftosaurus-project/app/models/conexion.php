9d58baf2a214c7c2bbb9507c88829d963c096bf0
LuckySoftware <99148157+LuckySoftware@users.noreply.github.com>
2025-06-21 17:52:20 -0300

    initial connection file for DB
    - deleted all the files that are not for pilot use
    
    - added the conexion.php file which will connect the code with the Db

---
+++ draftosaurus-project/app/models/conexion.php
@@ -0,0 +1,18 @@
+<?php
+
+    $host = "localhost";
+    $db = "draftosaurus";
+    $user = "root";
+    $pass = "";
+
+    try 
+    {
+        $conexion = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+     
+    catch (PDOException $e) 
+    {
+        die("Error de conexión: " . $e->getMessage());
+    }
+?>
