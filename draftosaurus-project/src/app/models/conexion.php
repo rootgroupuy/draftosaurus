2a71151841c59dba781abef27d8b9aa043cffad9
LuckySoftware <99148157+LuckySoftware@users.noreply.github.com>
2025-06-24 10:14:31 -0300

    refactor(views): improve code formatting and structure in HTML files

--- draftosaurus-project/src/app/models/conexion.php
+++ draftosaurus-project/src/app/models/conexion.php
@@ -1,16 +1,19 @@
 <?php
 
+    # DATOS DE LA CONEXION
     $host = "localhost";
     $db = "draftosaurus";
     $user = "root";
     $pass = "";
 
+    # CONECTAR A LA BASE DE DATOS
     try 
     {
         $conexion = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
      
+    # EN CASO DE ERROR
     catch (PDOException $e) 
     {
         die("Error de conexión: " . $e->getMessage());
